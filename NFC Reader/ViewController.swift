2192a8dc59217cb708e162fa3f716b3ba9fb419b
Leo Liu <asia.leoliu@gmail.com>
2018-04-26 15:38:01 +0800

    demo code complete

--- NFC Reader/ViewController.swift
+++ NFC Reader/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    private var nfcSession: NFCNDEFReaderSession?
+    private var nfcReadQueue = DispatchQueue(label: "com.leo.ios.tutorials.corenfc.NFC-Reader.nfcReadQueue")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nfcSession = NFCNDEFReaderSession(delegate: self, queue: nfcReadQueue, invalidateAfterFirstRead: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func scanPressed(_ sender: Any) {
+        if let nfcSession = self.nfcSession {
+            nfcSession.begin()
+        }
     }
+}
 
+// MARK: - NFCNDEFReaderSessionDelegate
 
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The NFCReaderSession was invalidated with error: \(error.localizedDescription)")
+        DispatchQueue.main.async {
+            self.messageLabel.text = error.localizedDescription
+        }
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        print("didDetectNDEFs!")
+        
+        print(messages)
+        
+        let result = messages[0].records.compactMap({ String(data: $0.payload.advanced(by: 3), encoding: .utf8) }).joined(separator: "\n")
+        DispatchQueue.main.async {
+            self.messageLabel.text = result
+        }
+    }
+    
 }
 
